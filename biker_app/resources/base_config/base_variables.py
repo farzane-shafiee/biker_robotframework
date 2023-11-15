cd1b69759fcf7ec65a1ac464d2524630da9c6d68
farzane shafiee <farzane.shafiee@zoodfood.com>
2023-11-15 15:52:42 +0330

    delete yaml file variables

--- biker_app/resources/base_config/base_variables.py
+++ biker_app/resources/base_config/base_variables.py
@@ -1,8 +1,3 @@
-# BROWSER = 'edge'
-# HEADLESS = False
-# URL = 'https://www.google.com/'
-# select_language = 'xpath://*[text()="Tout accepter"]'
-
 platformName = "Android"
 deviceName = "emulator-5554"
 appPackage = "com.zoodfood.express.stage"
