8b44ff60f5d245dda5d6285b03990de30d62e778
farzane shafiee <farzane.shafiee@zoodfood.com>
2023-12-18 13:51:30 +0330

    feature - change otp method

--- biker_app/resources/login_page/data_variables.py
+++ biker_app/resources/login_page/data_variables.py
@@ -1,9 +1,11 @@
-mobile_number_valid = "09193619468"
+mobile_number_valid = "09120000001"
 mobile_number_valid_without_zero = "9193619468"
 mobile_number_valid_with_code = "+989193619468"
 
 mobile_number_invalid = "09150560098"
 mobile_number_wrong = "09192"
-otp_invalid = "12345"
+
+otp_valid = "12345"
+otp_invalid = "11111"
 
 username = "f.shafiee"
